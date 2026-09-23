af0f51dee1667e9d2a09e41e82b08a169aff261d
TaijuHaneda <130955873+Taiju-h@users.noreply.github.com>
2026-09-24 06:41:29 +0900

    Load visitor gate styles in Canvas V2

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import HybridCanvasEditor from "@/components/hybrid-canvas-editor";
 import "./styles.css";
+import "./visitor.css";
 
 type Session = {
   authenticated: boolean;
